07309f24394a1b3c7d45ed0e27a75a67e02ff8a3
Furgl <jake.pollard15@gmail.com>
2017-09-11 17:21:00 -0500

    v2.4.8

--- src/main/java/twopiradians/blockArmor/common/item/ItemBlockArmor.java
+++ src/main/java/twopiradians/blockArmor/common/item/ItemBlockArmor.java
@@ -139,7 +139,8 @@ public class ItemBlockArmor extends ItemArmor
 				(!world.isRemote && entity instanceof EntityPlayer && stack.hasTagCompound() &&
 						stack.getTagCompound().hasKey("devSpawned") && !CommandDev.DEVS.contains(entity.getPersistentID()) &&
 						((EntityPlayer)entity).inventory.getStackInSlot(slot) == stack)) {
-			((EntityPlayer)entity).inventory.setInventorySlotContents(slot, ItemStack.EMPTY);
+			if (((EntityPlayer)entity).inventory.getStackInSlot(slot) == stack)
+				((EntityPlayer)entity).inventory.setInventorySlotContents(slot, ItemStack.EMPTY);
 			return;
 		}
 
